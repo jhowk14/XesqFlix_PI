1428de6774fae9a51d033203eba0a66e51cf754d
Jonathan Henrique <131157403+jhowk14@users.noreply.github.com>
2023-09-05 14:07:46 -0300

    Update [id].ts

--- pages/api/admin/delete/movie/[id].ts
+++ pages/api/admin/delete/movie/[id].ts
@@ -1,6 +1,6 @@
 
 import { NextApiRequest, NextApiResponse } from 'next';
-
+import prismadb from '@/libs/prismadb';
 
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
