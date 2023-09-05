9a6d0aef4c3d07edea840379d5a04ff05dd9ac30
Jonathan Henrique <131157403+jhowk14@users.noreply.github.com>
2023-09-05 14:07:03 -0300

    Update index.ts

--- pages/api/admin/movie/index.ts
+++ pages/api/admin/movie/index.ts
@@ -1,6 +1,6 @@
 
 import { NextApiRequest, NextApiResponse } from 'next';
-
+import prismadb from '@/libs/prismadb';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method === 'GET') {
