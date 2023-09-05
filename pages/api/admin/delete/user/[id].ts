ec1a737a8cc7c360a7b04bfb5ed22420b284560d
Jonathan Henrique <131157403+jhowk14@users.noreply.github.com>
2023-09-05 14:07:58 -0300

    Update [id].ts

--- pages/api/admin/delete/user/[id].ts
+++ pages/api/admin/delete/user/[id].ts
@@ -1,7 +1,7 @@
 
 import { NextApiRequest, NextApiResponse } from 'next';
 
-
+import prismadb from '@/libs/prismadb';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method === 'DELETE') {
