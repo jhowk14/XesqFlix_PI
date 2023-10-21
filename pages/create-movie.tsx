5ac1702bc3434130644dce28a98d83f7d0376297
Jonathan Henrique <131157403+jhowk14@users.noreply.github.com>
2023-10-21 11:39:06 -0300

    21/10

--- pages/create-movie.tsx
+++ pages/create-movie.tsx
@@ -9,7 +9,7 @@ import axios from 'axios';
 
 export async function getServerSideProps(context: NextPageContext) {
   const session = await getSession(context);
-  const {data} = await axios.get(`/api/admin/user/email/${session?.user?.email}`)
+  const {data} = await axios.get(`xesqflix.vercel.app/api/admin/user/email/${session?.user?.email}`)
   if (!session) {
     return {
       redirect: {
